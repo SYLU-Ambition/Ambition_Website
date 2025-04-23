43f4291129c7bf994a3b0dccba225286850958de
战空 <2276539496@qq.com>
2025-04-23 21:38:50 +0800

    first commit

---
+++ .vitepress/sidebar.mjs
@@ -0,0 +1,20 @@
+export default [//左侧边栏
+    {
+        text: '战队介绍',
+        collapsed: true,
+        items: [
+            { text: '战队历代成员', link: '/Ambition_Introduction/team_members' },
+            { text: '战队历史', link: '/Ambition_Introduction/team_history' },
+            { text: '宣传视频', link: '/Ambition_Introduction/promotional_video' } 
+        ]
+    },
+    {
+        text: '加入我们',
+        collapsed: true,
+        items: [
+            { text: '组别介绍', link: '/Sign_Up/group_introduction' },
+            { text: '招新通道', link: '/Sign_Up/sign_up' },
+            { text: 'Q&A', link: '/Sign_Up/Q&A' }
+        ]
+    }
+]
